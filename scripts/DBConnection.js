4834bbe27f8575c556c5d9251a0daf636bc42ff3
Radoslav Dimitrov <94966574+zoom-r@users.noreply.github.com>
2025-01-31 09:45:58 +0000

    Refactor database connection handling and add user access check functions

--- scripts/DBConnection.js
+++ scripts/DBConnection.js
@@ -1,9 +1,11 @@
-function test(){
-  var DBConnection = Jdbc.getCloudSqlConnection('jdbc:google:mysql://school-administration-444506:europe-west3:schooladministration2025/schools', 'admin', '{J,Yq<M,gN4vE7rT')
-  Logger.log(DBConnection);
-  DBConnection.close();
+function createDBConnection(){
+  try {
+    let conn = Jdbc.getCloudSqlConnection('jdbc:google:mysql://school-administration-444506:europe-west3:schooladministration2025/schools', 'admin', '{J,Yq<M,gN4vE7rT');
+    Logger.log('Database connection created');
+    return conn;
+  } catch (e) { //TODO: redirect to error page
+    Logger.log('Failed to create database connection: ' + e.message);
+    return null;
+  }
 }
-function test2(){
-  Logger.log(Utilities.getUuid());
-  return Utilities.getUuid();
-}
+
